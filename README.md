d87a6241ee7a9b244da78399ebc9fc81e1f65b2e
aayache-brahim <brahimayache123@gmail.com>
2024-04-15 02:01:47 +0100

    Your commit message

---
+++ README.md
@@ -0,0 +1,11 @@
+# Laravel Ecommerce
+
+Hi friends, today i hope we get a great day and this repo is a build Simple Ecommerce with laravel, this app ordering without login customer can check order with unique code. This app has only one user is admin to manage thier products customer can't register and login. Specialy for UI for client i use Atomic Design Structure and for admin UI i use beautiful template from [Mazer](https://github.com/zuramai/mazer). 
+
+Social Media : \
+[Instagram](https://www.instagram.com/bedddev/) \
+[Dribbble](https://dribbble.com/bedddev) \
+[Youtube](https://www.youtube.com/channel/UC_XQkWu_EPqam4vHdvh058A)
+
+hope you guys like it\
+Thanks a lot![Thumbnail](https://user-images.githubusercontent.com/78606852/178409940-52907ab3-73df-4d5d-9dec-fa448b39c0d5.png)
---
+++ resources/views/client/components/molecules/about/address.blade.php
@@ -0,0 +1,14 @@
+<div class="address py-md-5 py-2" id="address">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+        <h3 class="font-primary"><u>Address</u></h3>
+      </div>
+      <div class="col-lg-8 col-md-8 col-12">
+        <div class="responsive-map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15210.403551575462!2d-13.192272661714734!3d27.14788968595182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc3772e875e70709%3A0xf531b53e972f25d!2z2KzZiNmH2LHYqSDYp9mE2K3YsdmF2YrZhiDYp9mE2LnZitmI2YYgSmFvdWhhcmF0IEFsaGFyYW1haW4gTGHDonlvdW5l!5e0!3m2!1sar!2s!4v1713136695123!5m2!1sar!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
